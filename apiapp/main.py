804ba0196f86d423ae6be20e8bb00db00a1598cf
Jon Nguyen <jon.mail@hotmail.com>
2020-12-07 16:36:35 -0800

    completed presentation draft

--- apiapp/main.py
+++ apiapp/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from joblib import load
+import pandas as pd
 
 app = FastAPI()
-
+lr_model = load("lr_classifier.joblib")
 
 @app.get("/")
 async def root():
@@ -15,19 +16,29 @@ def predict(
     browser_family:str='Chrome', user_State:str='KS', 
     pageviews_before_popup:int=6, time_before_popup:float=0.0041,
     hits_before_popup:int=13, loyalty_user:bool=False
-):
+    ):
     """
-    Predict price based on year, manufacturer, model
+    Predict probability of upsell based on many factors
     """
+
     
-    
-    input = pd.DataFrame({
-        "year": [year],
-        "manufacturer": [manufacturer],
-        "model": [model_fz],
-        "odometer": 10000
+    df = pd.DataFrame({
+        'day_of_week': [day_of_week],
+        'hour': [hour],
+        'visit_day': [visit_day],
+        'visit_num': [visit_num],
+        'First_Visit': [First_Visit],
+        'IPD': [IPD],
+        'operating_system_family': [operating_system_family],
+        'browser_family': [browser_family],
+        'user_State': [user_State],
+        'pageviews_before_popup': [pageviews_before_popup],
+        'time_before_popup': [time_before_popup],
+        'hits_before_popup': [hits_before_popup],
+        'loyalty_user': [loyalty_user]
         })
     
-    pred = test_model.predict(input)
+    pred = lr_model.predict_proba(df)
+    probability = pred[0][1]
 
-    car_price_prediction = pred[0]
+    return {'Percent chance of upsell': round(probability*100, 2)}
